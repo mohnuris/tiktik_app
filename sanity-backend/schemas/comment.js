eabf23b807428d5288f2eb01981e01b693f78e6f
MohNuris <53099995+mohnuris@users.noreply.github.com>
2022-07-03 02:41:58 +0700

    install

---
+++ sanity-backend/schemas/comment.js
@@ -0,0 +1,17 @@
+export default {
+    name: 'comment',
+    title: 'Comment',
+    type: 'document',
+    field: [
+        {
+            name: 'postBy',
+            title: 'Posted BY',
+            type: 'postBy'
+        },
+        {
+            name: 'comment',
+            title: 'Comment',
+            type: 'string',
+        }
+    ]
+}
---
+++ sanity-backend/schemas/user.js
@@ -0,0 +1,17 @@
+export default {
+    name: 'user',
+    title: 'user',
+    type: 'document',
+    field: [
+        {
+            name: 'userName',
+            title: 'user Name',
+            type: 'string'
+        },
+        {
+            name: 'image',
+            title: 'Image',
+            type: 'string',
+        }
+    ]
+}
